b5da9981a4fdd12c5eac22b7c1b92a0a8b71dc2e
ondrejkrejci <ondrej.krejci@aalto.fi>
2019-10-29 12:31:29 +0200

    extract_desities and fitPauli repaired

--- extract_desities.py
+++ extract_desities.py
@@ -11,25 +11,26 @@ import pyProbeParticle.basUtils      as BU
 from   optparse import OptionParser
 
 parser = OptionParser()
-parser.add_option( "-i",     action="store", type="string", help="input file",                   default='CHGCAR.xsf'    )
-parser.add_option( "--zmin", action="store", type="float", help="tilt of tip electrostatic field (radians)", default=0.0 )
-parser.add_option( "--zmax", action="store", type="float", help="tilt of tip electrostatic field (radians)", default=6.0 )
-parser.add_option( "--dz",   action="store", type="float", help="tilt of tip electrostatic field (radians)", default=0.1 )
-parser.add_option("--plot", action="store_false",  help="plot extracted ?", default=True )
+parser.add_option( "-i",     action="store", type="string",  help="input file",                                 default='CHGCAR.xsf' )
+parser.add_option( "-n", "--same_name",action="store_false", help="save the file with same name as input file", default= True        )
+parser.add_option( "--zmin", action="store", type="float",   help="From what height to extract densities",      default=+0.0         )
+parser.add_option( "--zmax", action="store", type="float",   help="To which height extract densities",          default=-1.0         )
+parser.add_option( "--dz",   action="store", type="float",   help="use dz with step",                           default=-0.1         )
+parser.add_option( "--plot", action="store_false"        ,   help="plot extracted ?",                           default=True         )
 (options, args) = parser.parse_args()
 
 fname, fext = os.path.splitext( options.i ); fext = fext[1:]
-
 
 atoms,nDim,lvec     = BU.loadGeometry( options.i, params=PPU.params )
 GU.lib.setGridN   ( np.array( nDim[::-1], dtype=np.int32 )   )
 GU.lib.setGridCell( np.array( lvec[1:],   dtype=np.float64 ) )
 
-#zs = np.linspace( 0, lvec[3,2], nDim[0] )
-zs = np.arange( options.zmin, options.zmax, options.dz )
-#print lvec
-
 F,lvec,nDim=GU.load_scal_field(fname,data_format=fext)
+
+#zs = np.linspace( 0, lvec[3,2], nDim[0] )
+zs = np.arange( options.zmin, options.zmax if options.zmax > 0.0 else lvec[3,2], options.dz if options.dz > 0.0 else lvec[3,2]/nDim[0] )
+print lvec
+
 
 if( fext == 'cube' ):
 	F /= GU.Hartree2eV
@@ -54,6 +55,7 @@ for i in range( natoms ):
 
 #byType = zip(byType.items())
 #byType =  zip(*byType.items())
+fname = "atom_density" if options.same_name else fname
 byType = list(map(list, zip(*byType.items())))
 ntypes = len(byType[0])
 for i in range(ntypes):   byType[1][i] = byType[1][i][0] / byType[1][i][1]
--- fitPauli.py
+++ fitPauli.py
@@ -9,11 +9,17 @@ import matplotlib.pyplot as plt
 import pyProbeParticle               as PPU
 #import pyProbeParticle.GridUtils     as GU
 import pyProbeParticle.basUtils      as BU
-import matplotlib.pyplot as plt
 from   optparse import OptionParser
 
 parser = OptionParser()
-parser.add_option( "-i",   action="store", type="string", help="input file",                   default='OutFz'        )
+parser.add_option( "-i",           action="store", type="string", help="input file",                              default= 'CHGCAR'        )
+parser.add_option( "-o",           action="store", type="string", help="output xyz file name",                    default= 'new_xyz.xyz'   )
+parser.add_option( "-z", "--zcut", action="store", type="float",  help="cut-out atoms bellow this height",        default= -100.           )
+parser.add_option( "--height",     action="store", type="float",  help="how far above atom is isosurface fitted", default= +5.0            )
+parser.add_option( "--old",        action="store_true",           help="use old version of params.ini",           default= False	   )
+parser.add_option( "--plot",       action="store_false"        ,  help="not-plot ?",                              default= True            )
+parser.add_option( "--debug",      action="store_true"         ,  help="plot and pr. all lines and data from fit",default= False           )
+
 (options, args) = parser.parse_args()
 
 # ====== functions
@@ -25,12 +31,24 @@ def fitExp( zs, fs, zmin, zmax ):
     A     = np.exp( f0 - alpha*zmin )
     return alpha, A
 
-def getIsoArg( zs, fs, iso=0.01 ):
-    i = np.searchsorted( -fs, -iso )
-    x0 = zs[i-1]
-    f0 = fs[i-1]
-    dx = (zs[i] - x0 )
-    df = (fs[i] - f0)
+def getIsoArg( zs_, fs, iso=0.01, atom_z=0.0 ):
+    zi = int((atom_z -zs_[0]) // (zs_[1]-zs_[0]))
+    ai = int((atom_z+options.height +0.1 - zs_[0]) // (zs_[1]-zs_[0]))
+    zs = zs_ - atom_z
+    i = np.searchsorted( -fs[zi:ai], -iso )
+    #i = np.searchsorted( fs[:160], iso , side='right') # Not working either way
+    x0 = zs[zi+i-1]
+    f0 = fs[zi+i-1]
+    dx = (zs[zi+i] - x0 )
+    df = (fs[zi+i] - f0)
+    if options.debug or x0 +  dx*( iso - f0 )/df >= 5.0 or x0 +  dx*( iso - f0 )/df <= 1.0:
+        print "atom_z", atom_z
+        print "zs_[0]", zs_[0]
+        print "(atom_z -zs_[0])", (zs_[1]-zs_[0])
+        print "(zs_[1]-zs_[0])", (zs_[1]-zs_[0])
+        print(x0, f0, dx, df, i, zi, ai)
+        plt.plot(zs_[zi:ai],fs[zi:ai],[zs_[zi],zs_[ai]],[iso,iso])
+        plt.show()
     return x0 +  dx*( iso - f0 )/df
 
 def getMorse( r, R0=3.5, eps=0.03, alpha=-1.8,  cpull=1.0, cpush=1.0 ):
@@ -45,13 +63,10 @@ def getLJ( r, R0=3.5, eps=0.03, cpull=1.0, cpush=1.0 ):
 
 fname, fext     = os.path.splitext( options.i ); fext = fext[1:]
 atoms,nDim,lvec = BU.loadGeometry( options.i, params=PPU.params )
-atoms_z = atoms[3]
-atoms_e = atoms[0]
+
 data            = np.transpose( np.genfromtxt("atom_density_zlines.dat") )
 
 zs_bare = data[0]
-
-
 
 zmin = 1.2
 zmax = 2.2
@@ -60,23 +75,55 @@ zmax = 2.2
 
 FFparams = PPU.loadSpecies( fname=None )
 
+# remove atoms lower than zcut:
+mask = np.array(atoms[3]) >= float(options.zcut) ;
+
+for i in range(len(atoms)):
+    atoms[i] = np.array(atoms[i])[mask];
+
+atoms_z = atoms[3]
+atoms_e = atoms[0]
 iZs = atoms[0]
 
 REAs     = PPU.getSampleAtomsREA( iZs, FFparams )
 
+print "REAs:"
 print REAs
+print
 
+mask = np.append([True], mask)
+
+data = data[mask]
+
+del mask;
+
+if options.plot:
+    import matplotlib.pyplot as plt
+
+# f1 . pseudo-xyz file with all atoms about z-cut
 ilist =  range( len(atoms[0]) )
-#ilist = [0]
-#ilist = [0,18,26]
+f1 = open(options.o,"w")
+f1.write(str(len(atoms[0])) + '\n')
+f1.write('\n')
+# f2 - atomtypes.ini file with Riso (and Alpha) coresponding to each atom
+f2 = open("atomtypes.ini","w")
 for i in ilist:
     fs = data[1+i]
-    zs = zs_bare - atoms_z[i]
-    alpha, A = fitExp( zs, fs, zmin, zmax )
+    zs = zs_bare #- atoms_z[i]
+    alpha, A = fitExp( zs - atoms_z[i] , fs, zmin, zmax )
 
-    Riso = getIsoArg( zs, fs, iso=0.017 )
-    plt.axvline(Riso)
-    print " elem %i a_z %f Riso %f alpha %f alpha/2 %f" %( atoms_e[i], atoms_z[i], Riso, alpha, alpha/2.0 ), REAs[i]
+    Riso = getIsoArg( zs, fs, iso=0.017, atom_z=atoms_z[i] )
+    if not (0.5 < Riso < 5.0) :
+        print "!!! Problem with Riso for atom no. %i : Riso %f, we will use tabled number." %(i,Riso)
+        Riso = REAs[i][0]
+    f1.write(str(i+1)+' '+str(atoms[1][i])+' '+str(atoms[2][i])+' '+str(atoms[3][i])+'\n')
+    if options.old: # old verison of atomtypes.inis
+        f2.write(str(Riso)+' '+str(REAs[i][1])+' '+str(i+1)+' '+FFparams[iZs[i]-1][4]+str(i)+'\n')
+    else: # ocl version of params.ini
+        f2.write(str(Riso)+' '+str(REAs[i][1])+' '+str(alpha/2)+' '+str(i+1)+' '+FFparams[iZs[i]-1][4]+str(i)+'\n')
+    #plt.axvline(Riso)
+    #print " elem %i a_z %f Riso %f alpha %f alpha/2 %f" %( atoms_e[i], atoms_z[i], Riso, alpha, alpha/2.0 ), REAs[i]
+    print " elem %i a_z %f Riso %f " %( atoms_e[i], atoms_z[i], Riso ), REAs[i]
 
     REAs[i][0] = Riso
     REAs[i][2] = alpha/2.0
@@ -91,18 +138,23 @@ for i in ilist:
     print " elem %i a_z %f alpha %f alpha/2 %f A  %f d2p %f " %( atoms_e[i], atoms_z[i], alpha, alpha*0.5, A, dens2Pauli )
     '''
     
-    plt.plot( zs, fs, label=("%i" %atoms_e[i])  )
-    #plt.plot( zs, A*np.exp( alpha*zs ) )
-    #plt.plot( zs, dens2Pauli*A*np.exp( alpha*zs ) )
-    #plt.plot( zs, getMorse(zs, R0=1.8, eps=0.03, alpha=-1.8, cpull=0.0) )
+    if options.plot:
+         plt.plot( zs - atoms_z[i], fs, label=("%i" %atoms_e[i])  )
+         #plt.plot( zs, A*np.exp( alpha*zs ) )
+         #plt.plot( zs, dens2Pauli*A*np.exp( alpha*zs ) )
+         #plt.plot( zs, getMorse(zs, R0=1.8, eps=0.03, alpha=-1.8, cpull=0.0) )
 
-plt.legend()    
-plt.xlim(0.0, 5.0)
-plt.ylim(1e-8, 1e+5)
-plt.yscale("log")
-plt.axvline( zmin, ls="--", c="k" ); plt.axvline( zmax, ls="--", c="k" )
-plt.grid()
+f1.close()
+f2.close()
 
+if options.plot:
+    plt.plot( [0.0,5.0],[0.017,0.017], label=("threshold") )
+    plt.legend()    
+    plt.xlim(0.0, options.height)
+    plt.ylim(1e-8, 1e+5)
+    plt.yscale("log")
+    plt.axvline( zmin, ls="--", c="k" ); plt.axvline( zmax, ls="--", c="k" )
+    plt.grid()
 
 atoms = np.transpose( np.array(atoms) )
 print atoms.shape, REAs.shape
@@ -130,5 +182,5 @@ plt.axvline( eps, ls="--", c="k" ); plt.axhline( R0, ls="--", c="k" )
 plt.grid()
 '''
 
-
-plt.show()
+if options.plot:
+    plt.show()
