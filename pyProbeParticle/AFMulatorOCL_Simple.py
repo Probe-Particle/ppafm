Fixed output units in AFMulator + small optimization.
The output units in AFMulator were not taking into account the cantilever parameters and a unit conversion factor was missing as well. Added the cantilever resonance frequency and stiffness as adjustable parameters to the AFMulator class init.

The Hartree potential gradient calculation now checks if the potential grid already matches the force field grid, and if it does then it uses a simpler kernel that does not need to use interpolation.

pyProbeParticle/AFMulatorOCL_Simple.py
@@ -34,6 +34,8 @@ class AFMulator():
         npbc: tuple of three ints. How many periodic images of atoms to use in (x, y, z) dimensions. Used for calculating
             Lennard-Jones force field and electrostatic field from point charges. Electrostatic field from a Hartree
             potential defined on a grid is always considered to be periodic.
+        f0Cantilever: float. Resonance frequency of cantilever in Hz.
+        kCantilever: float. Harmonic spring constant of cantilever in N/m.
         initFF: Bool. Whether to initialize buffers. Set to False to modify force field and scanner parameters
             before initialization.
     '''
@@ -69,6 +71,8 @@ class AFMulator():
         tipR0           = [0.0, 0.0, 3.0],
         tipStiffness    = [0.25, 0.25, 0.0, 30.0],
         npbc            = (1, 1, 0),
+        f0Cantilever    = 30300,
+        kCantilever     = 1800,
         initFF          = True 
     ):
 
@@ -84,6 +88,8 @@ class AFMulator():
         self.iZPP = iZPP
         self.df_steps = df_steps
         self.tipR0 = tipR0
+        self.f0Cantilever = f0Cantilever
+        self.kCantilever = kCantilever
         self.npbc = npbc
 
         self.typeParams = hl.loadSpecies('atomtypes.ini')
@@ -93,7 +99,7 @@ class AFMulator():
 
         self.scanner = oclr.RelaxedScanner()
         self.scanner.relax_params = np.array( self.relaxParams, dtype=np.float32 )
-        self.scanner.stiffness = np.array(tipStiffness, dtype=np.float32) / -16.0217662
+        self.scanner.stiffness = np.array(tipStiffness, dtype=np.float32) / -PPU.eVA_Nm
 
         if initFF:
             self.initFF()
@@ -139,6 +145,7 @@ class AFMulator():
         # Set df convolution weights
         self.dz = (self.scan_window[1][2] - self.scan_window[0][2]) / self.scan_dim[2]
         self.dfWeight = PPU.getDfWeight(self.df_steps, dz=self.dz)[0].astype(np.float32)
+        self.dfWeight *= PPU.eVA_Nm * self.f0Cantilever / self.kCantilever
         self.amplitude = self.dz * len(self.dfWeight)
 
         # Initialize force field

pyProbeParticle/fieldOCL.py
@@ -636,7 +636,7 @@ class ForceField_LJC:
 
         return FE
 
-    def run_gradPotentialGrid(self, pot=None, E_field=None, h=None, local_size=(32,), bCopy=True, bFinish=True):
+    def run_gradPotential(self, pot=None, E_field=None, h=None, local_size=(32,), bCopy=True, bFinish=True):
         '''
         Obtain electric field on the force field grid as the negative gradient of Hartree potential
         via centered difference.
@@ -646,8 +646,6 @@ class ForceField_LJC:
                 beforehand with prepareBuffers.
             E_field: np.ndarray or None. Array where output electric field is copied to if bCopy == True.
                 If None and bCopy == True, will be created automatically.
-            h: float > 0.0 or None. Finite difference step size (one-sided) in angstroms. If None, the default
-                value DEFAULT_FD_STEP is used.
             local_size: tuple of a single int. Size of local work group on device.
             bCopy: Bool. Whether to return the calculated electric field to host.
             bFinish: Bool. Whether to wait for execution to finish.
@@ -698,6 +696,91 @@ class ForceField_LJC:
             self.lvec0,
             np.array([h, h, h, 0.0], dtype=np.float32)
         )
+
+        if bCopy: cl.enqueue_copy(self.queue, E_field, self.cl_Efield)
+        if bFinish: self.queue.finish()
+        if bRuntime: print("runtime(ForceField_LJC.run_gradPotentialGrid) [s]: ", time.perf_counter() - t0)
+
+        return E_field
+
+    def run_gradPotentialGrid(self, pot=None, E_field=None, h=None, local_size=(32,), bCopy=True, bFinish=True):
+        '''
+        Obtain electric field on the force field grid as the negative gradient of Hartree potential
+        via centered difference.
+
+        Arguments:
+            pot: HartreePotential or None. Hartree potential to differentiate. If None, has to be initialized
+                beforehand with prepareBuffers.
+            E_field: np.ndarray or None. Array where output electric field is copied to if bCopy == True.
+                If None and bCopy == True, will be created automatically.
+            h: float > 0.0 or None. Finite difference step size (one-sided) in angstroms. If None, the default
+                value DEFAULT_FD_STEP is used.
+            local_size: tuple of a single int. Size of local work group on device.
+            bCopy: Bool. Whether to return the calculated electric field to host.
+            bFinish: Bool. Whether to wait for execution to finish.
+
+        Returns: np.ndarray if bCopy == True or None otherwise.
+        '''
+
+        if bRuntime: t0 = time.perf_counter()
+
+        mf = cl.mem_flags
+        if pot:
+            assert isinstance(pot, HartreePotential), 'pot should be a HartreePotential object'
+            if self.cl_pot:
+                if self.cl_pot.size < pot.array.nbytes:
+                    print('Warning: potential grid size larger than existing device buffer. Reallocating.')
+                    self.cl_pot.release()
+                    self.cl_pot = cl.Buffer(self.ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=pot.array)
+                else:
+                    self.updateBuffers(pot=pot.array)
+            else:
+                self.cl_pot = cl.Buffer(self.ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=pot.array)
+        elif not self.cl_pot:
+            raise ValueError("Hartree potential not initialized on the device. "
+                "Either initialize it with prepareBuffers or pass it here as a HartreePotential object.")
+        
+        if bCopy:
+            E_field = E_field or np.empty(self.nDim, dtype=np.float32)
+            if not np.allclose(E_field.shape, self.nDim):
+                raise ValueError(f'E_field array dimensions {E_field.shape} do not match with '
+                    f'force field dimensions {self.nDim}.')
+
+        if not self.cl_Efield:
+            self.cl_Efield = cl.Buffer(self.ctx, mf.READ_WRITE, size=4*np.prod(self.nDim))
+
+        h = h or DEFAULT_FD_STEP
+
+        # Check if potential grid matches the force field grid. If it does, we don't need to do interpolation.
+        matching_grid = (
+            np.allclose(self.pot.shape, self.nDim[:3]) and
+            (np.abs(self.pot.origin - self.lvec0[:3]) < 1e-3).all() and
+            (np.abs(np.diag(self.pot.step * self.pot.shape) - self.lvec[:, :3]) < 1e-3).all()
+        )
+        print('Matching grid:', matching_grid)
+
+        if bRuntime: print("runtime(ForceField_LJC.run_gradPotentialGrid.pre) [s]: ", time.perf_counter() - t0)
+
+        global_size = [int(np.ceil(np.prod(self.nDim[:3]) / local_size[0]) * local_size[0])]
+        if matching_grid:
+            cl_program.gradPotential(self.queue, global_size, local_size,
+                self.cl_pot,
+                self.cl_Efield,
+                np.append(self.pot.shape, 0).astype(np.int32),
+                np.append(self.pot.step, 0).astype(np.float32)
+            )
+        else:
+            cl_program.gradPotentialGrid(self.queue, global_size, local_size,
+                self.cl_pot,
+                self.cl_Efield,
+                np.append(self.pot.shape, 0).astype(np.int32),
+                np.append(self.pot.step, 0).astype(np.float32),
+                np.append(self.pot.origin, 0).astype(np.float32),
+                self.nDim,
+                np.array([self.dlvec[0, 0], self.dlvec[1, 1], self.dlvec[2, 2], 0], dtype=np.float32),
+                self.lvec0,
+                np.array([h, h, h, 0.0], dtype=np.float32)
+            )
 
         if bCopy: cl.enqueue_copy(self.queue, E_field, self.cl_Efield)
         if bFinish: self.queue.finish()