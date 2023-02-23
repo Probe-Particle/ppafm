dfd71b1cc33d3010a61c1824c1b97135b0bcc64f
NikoOinonen <nikooin@gmail.com>
2023-02-23 16:48:05 +0200

    Testing an alternate way of building the extensions with setuptools.

--- setup.py
+++ setup.py
@@ -1,23 +1,42 @@
 
 import os
 from setuptools import Extension, setup
-from setuptools.command.build import build
+# from setuptools.command.build import build
 
-def make():
-    package_path = os.path.dirname(os.path.realpath(__file__))
-    cpp_path = os.path.normpath(package_path + '/ppafm/cpp/')
-    current_directory = os.getcwd()
-    os.chdir (cpp_path)
-    os.system("make clean")
-    os.system("make all")
-    os.chdir (current_directory)
+# def make():
+#     package_path = os.path.dirname(os.path.realpath(__file__))
+#     cpp_path = os.path.normpath(package_path + '/ppafm/cpp/')
+#     current_directory = os.getcwd()
+#     os.chdir (cpp_path)
+#     os.system("make clean")
+#     os.system("make all")
+#     os.chdir (current_directory)
 
-class Build(build):
-    '''Custom build for setuptools to compile C shared libraries.'''
-    def run(self):
-        make()
-        super().run()
+# class Build(build):
+#     '''Custom build for setuptools to compile C shared libraries.'''
+#     def run(self):
+#         make()
+#         super().run()
+
+# setup(
+#     cmdclass={'build': Build}
+# )
+
+from setuptools.command.build_ext import build_ext
+
+class BuildExt(build_ext):
+    def get_ext_filename(self, ext_name):
+        return ext_name.replace('.', '/') + '_lib.so'
+
+cpp_modules = ['ProbeParticle', 'GridUtils', 'fitSpline', 'fitting']
 
 setup(
-    cmdclass={'build': Build}
+    ext_modules=[
+        Extension(
+            name=f'ppafm.cpp.{module}',
+            sources=[f'ppafm/cpp/{module}.cpp']
+        )
+        for module in cpp_modules
+    ],
+    cmdclass={'build_ext': BuildExt},
 )
