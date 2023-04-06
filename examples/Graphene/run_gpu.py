edc2c08945b385c3bd30718ad87d91b11f9d63c1
NikoOinonen <nikooin@gmail.com>
2023-04-06 14:14:21 +0300

    Removed unnecessary trick with lattice vectors.

--- examples/Graphene/run_gpu.py
+++ examples/Graphene/run_gpu.py
@@ -9,11 +9,5 @@ xyzs, Zs, qs, _ = loadXYZ('./Gr6x6N3hole.xyz')
 # Create simulator and load parameters
 afmulator = AFMulator.from_params('./params.ini')
 
-# This is bit of a hack: the afmulator does not fully support non-rectangular cells so
-# we first grab the sample lvec we read from params.ini and then reset the afmulator
-# force field lvec to rectangular.
-pbc_lvec = afmulator.lvec
-afmulator.setLvec()
-
 # Run simulation and plot
-afmulator(xyzs, Zs, qs, pbc_lvec=pbc_lvec, plot_to_dir='./afm_ocl')
+afmulator(xyzs, Zs, qs, plot_to_dir='./afm_ocl')
