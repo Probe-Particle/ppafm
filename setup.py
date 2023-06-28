cf6a6f166f4ad9acefa23f4e9abd74e3909f3e67
NikoOinonen <nikooin@gmail.com>
2023-06-28 18:33:36 +0300

    Cleaning up.

--- setup.py
+++ setup.py
@@ -17,36 +17,23 @@ class Build(build):
         self.current_directory = Path.cwd()
 
     def run(self):
+        os.chdir(self.cpp_path)
         if platform.system() == 'Windows':
             self.build_windows()
         else:
             self.make()
+        os.chdir(self.current_directory)
         super().run()
 
     def make(self):
-        os.chdir(self.cpp_path)
         os.system("make clean")
         os.system("make all")
-        os.chdir(self.current_directory)
 
     def build_windows(self):
-        os.chdir(self.cpp_path)
-        # vars_path = self.get_vars_path()
         for module in self.cpp_modules:
-            # cmd = f'"{vars_path}" /no_logo /arch=amd64 && cl.exe /O2 /D_USRDLL /D_WINDLL {module}.cpp /link /dll /out:{module}_lib.dll'
             cmd = f'cl.exe /O2 /D_USRDLL /D_WINDLL {module}.cpp /link /dll /out:{module}_lib.dll'
             print(cmd)
             os.system(cmd)
-        os.chdir(self.current_directory)
-
-    def get_vars_path(self):
-        vs_path = Path('C:/') / 'Program Files (x86)' / 'Microsoft Visual Studio'
-        if not vs_path.exists():
-            raise RuntimeError('Could not detect Microsoft Visual Studio installation')
-        vars_path = list(vs_path.glob('**/VsDevCmd.bat'))
-        if len(vars_path) == 0:
-            raise RuntimeError('Could not find VsDevCmd.bat')
-        return vars_path[0]
 
 setup(
     cmdclass={'build': Build},
