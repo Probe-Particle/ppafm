3a8041412491126bccd578385fbc76772e02a776
Martin Ondracek <ondracek@fzu.cz>
2023-09-25 20:16:33 +0200

    Correct test_get_df_weight in test_common.py

--- tests/test_common.py
+++ tests/test_common.py
@@ -4,27 +4,15 @@ from ppafm import common
 
 
 def test_get_df_weight():
-    #amplitude = dz = 0.1
+    # amplitude = dz = 0.1
     assert np.allclose(common.get_df_weight(0.1), np.array([-5.0, 5.0]))
-    
-    #amplitude=1.0, dz=0.2
+
+    # amplitude=1.0, dz=0.2
     w = common.get_df_weight(1.0, dz=0.2)
     assert np.allclose(
         w,
         np.array(
-            [
-                -2.60220097e-01,
-                -1.91452256e-01,
-                -9.80141360e-02,
-                -5.63847676e-02,
-                -2.62785481e-02,
-                3.69960354e-16,
-                2.62785481e-02,
-                5.63847676e-02,
-                9.80141360e-02,
-                1.91452256e-01,
-                2.60220097e-01,
-            ]
+            [-0.35594622, -0.22193265, -0.05447093, 0.05447093, 0.22193265, 0.35594622]
         ),
     )
 
