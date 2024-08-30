6985c4cd96460b724019f5221b5bc4bb406911a0
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2024-08-30 11:46:52 +0200

    Add test to the PpafmParameters class

--- tests/test_common.py
+++ tests/test_common.py
@@ -35,3 +35,52 @@ def test_sphere_tangent_space(n=2):
             ]
         ),
     )
+
+
+def test_ppafm_parameters():
+    # First, testing the default values
+    p_default = common.PpafmParameters()
+    assert p_default.PBC == True
+    assert p_default.nPBC == [1, 1, 1]
+    assert p_default.probeType == "O"
+    assert np.isclose(p_default.charge, 0.0)
+    assert p_default.ffModel == "LJ"
+    assert np.isclose(p_default.r0Probe, [0.0, 0.0, 4.0]).all()
+    assert p_default.gridN == [-1, -1, -1]
+    assert np.isclose(p_default.klat, 0.5)
+    assert np.isclose(p_default.gridA, [20.0, 0.0, 0.0]).all()
+    assert np.isclose(p_default.gridB, [0.0, 20.0, 0.0]).all()
+    assert np.isclose(p_default.gridC, [0.0, 0.0, 20.0]).all()
+    assert np.isclose(p_default.scanStep, 0.1).all()
+    assert np.isclose(p_default.scanMin, [0.0, 0.0, 5.0]).all()
+    assert np.isclose(p_default.scanMax, [20.0, 20.0, 8.0]).all()
+    assert np.isclose(p_default.Amplitude, 1.0)
+    assert np.isclose(p_default.aMorse, -1.6)
+    assert p_default.vdWDampKind == 2
+
+    # Load the parameters from a file
+    p_ini = common.PpafmParameters.from_file("data/test_params.ini")
+    assert p_ini.PBC == False
+    assert p_ini.nPBC == [2, 3, 4]
+    assert p_ini.probeType == "Xe"
+    assert np.isclose(p_ini.charge, 0.5)
+    assert p_ini.ffModel == "Morse"
+    assert np.isclose(p_default.r0Probe, [0.0, 0.0, 4.0]).all()
+    assert np.isclose(p_ini.gridN, [10, 20, 30]).all()
+    assert np.isclose(p_ini.klat, 1.5)
+    assert np.isclose(p_ini.gridA, [5.0, 6.0, 7.0]).all()
+    assert np.isclose(p_ini.gridB, [8.0, 9.0, 10.0]).all()
+    assert np.isclose(p_ini.gridC, [11.0, 12.0, 13.0]).all()
+    assert np.isclose(p_ini.scanStep, [0.1, 0.2, 0.3]).all()
+    assert np.isclose(p_ini.scanMin, [1.0, 2.0, 5.0]).all()
+    assert np.isclose(p_ini.scanMax, [21.0, 22.0, 9.0]).all()
+    assert np.isclose(p_ini.Amplitude, 8.0)
+    assert np.isclose(p_ini.aMorse, -2.6)
+    assert p_ini.vdWDampKind == 3
+
+    # Dump the parameters to a toml file
+    p_ini.to_file("data/test_params.toml")
+
+    # Load the parameters from the toml file and compare to the original
+    p_toml = common.PpafmParameters.from_file("data/test_params.toml")
+    assert p_ini == p_toml
