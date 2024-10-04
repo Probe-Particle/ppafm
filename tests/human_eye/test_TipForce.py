7b0ab923f653e17b68361196caed466a9a9eeec7
NikoOinonen <nikooin@gmail.com>
2024-10-04 13:10:02 +0300

    Move code in "human_eye" tests into functions.
    This seems to fix the segfault when running tests with pytest.

--- tests/human_eye/test_TipForce.py
+++ tests/human_eye/test_TipForce.py
@@ -7,46 +7,53 @@ import numpy as np
 
 from ppafm import common, core
 
-spline_ini = """
-0.0   0.4 -0.1
-2.0   0.2 -0.1
-4.0   0.0 -0.1
-5.0  -4.0 -4.0
-10.0 -24.0 -4.0
-"""
-# S = np.genfromtxt('TipRSpline.ini')
-S = np.genfromtxt(StringIO(spline_ini), skip_header=1)
 
-parameters = common.PpafmParameters()
+def test_tip_force():
 
-print("TipRSpline.ini overrides harmonic tip")
-xs = S[:, 0].copy()
-print("xs: ", xs)
-ydys = S[:, 1:].copy()
-print("ydys: ", ydys)
-plt.plot(xs, ydys[:, 0], "o")
-plt.plot(xs, ydys[:, 0] + ydys[:, 1], ".")
-core.setTipSpline(xs, ydys)
+    spline_ini = """
+    0.0   0.4 -0.1
+    2.0   0.2 -0.1
+    4.0   0.0 -0.1
+    5.0  -4.0 -4.0
+    10.0 -24.0 -4.0
+    """
+    # S = np.genfromtxt('TipRSpline.ini')
+    S = np.genfromtxt(StringIO(spline_ini), skip_header=1)
 
-core.setTip(parameters=parameters)
+    parameters = common.PpafmParameters()
 
-fs = np.zeros((60, 3))
-r0 = np.array([0.0, 0.0, 0.5])
-dr = np.array([0.0, 0.0, 0.1])
-R = np.array([0.0, 0.0, 0.0])
-xs = np.array(list(range(len(fs)))) * dr[2] + r0[2]
-# print "xs=",xs
+    print("TipRSpline.ini overrides harmonic tip")
+    xs = S[:, 0].copy()
+    print("xs: ", xs)
+    ydys = S[:, 1:].copy()
+    print("ydys: ", ydys)
+    plt.plot(xs, ydys[:, 0], "o")
+    plt.plot(xs, ydys[:, 0] + ydys[:, 1], ".")
+    core.setTipSpline(xs, ydys)
 
-print(">>>  core.test_force( 1, r0, dr, R, fs )")
-core.test_force(1, r0, dr, R, fs)
-plt.plot(xs, fs[:, 2])
+    core.setTip(parameters=parameters)
 
-print(">>>  core.test_force( 2, r0, dr, R, fs )")
-core.test_force(2, r0, dr, R, fs)
-plt.plot(xs, fs[:, 2])
+    fs = np.zeros((60, 3))
+    r0 = np.array([0.0, 0.0, 0.5])
+    dr = np.array([0.0, 0.0, 0.1])
+    R = np.array([0.0, 0.0, 0.0])
+    xs = np.array(list(range(len(fs)))) * dr[2] + r0[2]
+    # print "xs=",xs
 
-# print "fs:", fs
+    print(">>>  core.test_force( 1, r0, dr, R, fs )")
+    core.test_force(1, r0, dr, R, fs)
+    plt.plot(xs, fs[:, 2])
 
-plt.grid()
-plt.savefig("tip_force.png")
-plt.show()
+    print(">>>  core.test_force( 2, r0, dr, R, fs )")
+    core.test_force(2, r0, dr, R, fs)
+    plt.plot(xs, fs[:, 2])
+
+    # print "fs:", fs
+
+    plt.grid()
+    plt.savefig("tip_force.png")
+    plt.show()
+
+
+if __name__ == "__main__":
+    test_tip_force()
