139f98451aeb7173bc149ad5a8fc98890955d2f3
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2024-12-10 09:57:27 +0100

    Trying harder.

--- examples/pyridineDensOverlap/example_pyridine_density_overlap.py
+++ examples/pyridineDensOverlap/example_pyridine_density_overlap.py
@@ -31,7 +31,7 @@ def example_pyridine_density_overlap():
             zip_ref.extractall("tip")
 
     generate_conv_rho(["-s", "LOCPOT.xsf", "-t", "tip/density_CO.xsf", "-B", "1.0", "--energy"])
-    generate_elff(["-i", "LOCPOT.xsf", "--tip_dens", "tip/density_CO.xsf", "--Rcore", "0.7", "--energy", "--doDensity"])
+    generate_elff(["--input", "LOCPOT.xsf", "--tip_dens", "tip/density_CO.xsf", "--Rcore", "0.7", "--energy", "--doDensity"])
     generate_dftd3(["--input", "LOCPOT.xsf", "--df_name", "PBE"])
 
     relaxed_scan(
