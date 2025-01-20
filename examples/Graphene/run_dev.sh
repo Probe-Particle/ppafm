5bfacfdaff54d450b596118056c24d2243dfb907
ondrejkrejci <ondrej.krejci@aalto.fi>
2025-01-20 17:57:16 +0200

    removing a typo

--- examples/Graphene/run_dev.sh
+++ examples/Graphene/run_dev.sh
@@ -11,7 +11,7 @@ export OMP_NUM_THREADS
 
 # ======= STEP 1 : Generate force-field grid
 ppafm-generate-ljff -i Gr6x6N3hole.xyz
-Appafm-generate-elff-point-charges -i Gr6x6N3hole.xyz --tip s
+ppafm-generate-elff-point-charges -i Gr6x6N3hole.xyz --tip s
 
 # ======= STEP 2 : Relax Probe Particle using that force-field grid
 ppafm-relaxed-scan -k 0.5 -q -0.05
