ae17c0d7f62706b29c19294f9a54a5266a610c18
ondrejkrejci <ondrej.krejci@aalto.fi>
2025-01-20 15:58:15 +0200

    Adding the plotting of z-component of force; notes about the internal units and making the graphene dev example to be running fully.

--- examples/Graphene/run_dev.sh
+++ examples/Graphene/run_dev.sh
@@ -10,11 +10,11 @@ OMP_NUM_THREADS=$ncpu
 export OMP_NUM_THREADS
 
 # ======= STEP 1 : Generate force-field grid
-#ppafm-generate-ljff -i Gr6x6N3hole.xyz
-#ppafm-generate-elff-point-charges -i Gr6x6N3hole.xyz --tip s
+ppafm-generate-ljff -i Gr6x6N3hole.xyz
+Appafm-generate-elff-point-charges -i Gr6x6N3hole.xyz --tip s
 
 # ======= STEP 2 : Relax Probe Particle using that force-field grid
 ppafm-relaxed-scan -k 0.5 -q -0.05
 
 # ======= STEP 3 : Plot the results
-ppafm-plot-results -k 0.5 -q -0.05 -a 2.0 --df
+ppafm-plot-results -k 0.5 -q -0.05 -a 2.0 --df --Fz
