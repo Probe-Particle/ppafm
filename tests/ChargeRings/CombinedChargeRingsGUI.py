bcb49a36267a0833ff3cd86b9cf1acccd32e44ec
ProkopHapala <ProkopHapala@gmail.com>
2025-01-22 17:29:54 +0100

    improved CombinedChargeRingsGUI.py

--- tests/ChargeRings/CombinedChargeRingsGUI.py
+++ tests/ChargeRings/CombinedChargeRingsGUI.py
@@ -8,22 +8,23 @@ import matplotlib; matplotlib.use('Qt5Agg')
 from PyQt5 import QtCore, QtWidgets
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
-# from TipMultipole import (
-#     makeCircle, makeRotMats, compute_site_energies,
-#     compute_site_tunelling, makePosXY, compute_V_mirror
-# )
-
-import TipMultipole as tmul
-
+from TipMultipole import (
+    makeCircle, makeRotMats, compute_site_energies,
+    compute_site_tunelling, makePosXY, compute_V_mirror
+)
 
 class ApplicationWindow(QtWidgets.QMainWindow):
     def __init__(self):
         QtWidgets.QMainWindow.__init__(self)
         self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
-        self.setWindowTitle("Combined Charge Rings GUI")
+        self.setWindowTitle("Combined Charge Rings GUI v2")
         self.main_widget = QtWidgets.QWidget(self)
         
-        # Combined parameter specifications
+        # Initialize calculation results storage
+        self.tip_potential_data = None
+        self.qdot_system_data = None
+        
+        # Parameter specifications (same as before)
         self.param_specs = {
             # Tip Parameters
             'Q_tip':         {'group': 'Tip Parameters',    'widget': 'double', 'range': (-2.0, 2.0),  'value': 0.6, 'step': 0.1},
@@ -105,7 +106,7 @@ class ApplicationWindow(QtWidgets.QMainWindow):
                 if 'step' in spec:
                     widget.setSingleStep(spec['step'])
             
-            widget.valueChanged.connect(self.update_plots)
+            widget.valueChanged.connect(self.run_simulation)
             hb.addWidget(widget)
             self.param_widgets[param_name] = widget
         
@@ -127,6 +128,9 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         self.main_widget.setFocus()
         self.setCentralWidget(self.main_widget)
         self.init_simulation()
+
+        self.run_simulation()
+
     
     def get_param_values(self):
         """Get current values of all parameters"""
@@ -163,25 +167,132 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         R = params['radius']
         
         # Setup sites on circle using Python implementation
-        self.spos, phis = tmul.makeCircle(n=nsite, R=R)
+        self.spos, phis = makeCircle(n=nsite, R=R)
         self.spos[:,2] = params['z_tip']
         
         # Setup multipoles and site energies
         self.Esite = np.full(nsite, params['Esite'])
-        self.rots = tmul.makeRotMats(phis + params['phiRot'])
+        self.rots = makeRotMats(phis + params['phiRot'])
         
         # Initialize global parameters
         self.temperature = params['temperature']
         self.onSiteCoulomb = params['onSiteCoulomb']
     
+    def calculateTipPotential(self, params):
+        """Calculate tip potential data for X-Z projections"""
+        # X-Z grid
+        ps_xz, Xs, Zs = makePosXY(n=params['npix'], L=params['L'], axs=(0,2,1))
+        
+        # Tip position
+        zT = params['z_tip'] + params['Rtip']
+        tip_pos = np.array([0.0, 0.0, zT])
+        
+        # Calculate potentials
+        self.tip_potential_data = {
+            'Vtip':   compute_V_mirror(tip_pos, ps_xz,  VBias=params['VBias'],  Rtip=params['Rtip'], zV0=params['zV0']                       ).reshape(params['npix'], params['npix']),
+            'Esites': compute_site_energies( ps_xz, np.array([[0.0,0.0,params['zQd']]]),params['VBias'], params['Rtip'], zV0=params['zV0'] ).reshape(params['npix'], params['npix']),
+            'ps_xz':  ps_xz,
+            'extent': [-params['L'], params['L'], -params['L'], params['L']]
+        }
+    
+    def calculateQdotSystem(self, params):
+        """Calculate quantum dot system data for X-Y projections"""
+        # X-Y grid
+        ps_xy, Xs, Ys = makePosXY(n=params['npix'], L=params['L'], p0=(0,0,params['z_tip']))
+        
+        # Compute site energies and tunneling rates
+        Es = compute_site_energies( ps_xy, self.spos,VBias=params['Q_tip'], Rtip=1.0, zV0=params['z_tip'], E0s=self.Esite )
+        
+        T = compute_site_tunelling(ps_xy, self.spos, beta=params['decay'], Amp=1.0).reshape(params['npix'], params['npix'], -1)
+        
+        # Calculate charge distribution
+        Q = self.solve_occupancies(Es, T.reshape(-1, T.shape[-1]))
+        Q = Q.reshape(params['npix'], params['npix'], -1)
+        
+        # Compute STM and dI/dQ
+        I = self.getSTM_map(Q, T, params['dQ'])
+        dIdQ = (I - np.mean(I)) / params['dQ']
+        
+        self.qdot_system_data = { 'total_charge': np.sum(Q, axis=2), 'STM': I, 'dIdQ': dIdQ, 'ps_xy': ps_xy, 'extent': [-params['L'], params['L'], -params['L'], params['L']] }
+    
+    def plotTipPotential(self):
+        """Plot X-Z projections using precomputed data"""
+        data = self.tip_potential_data
+        params = self.get_param_values()
+        
+        # 1D Potential
+        self.ax1.clear()
+        x_coords = np.linspace(-data['extent'][1], data['extent'][1], data['Vtip'].shape[0])
+        self.ax1.plot(x_coords, data['Vtip'].mean(axis=1))
+        self.ax1.set_title("1D Potential")
+        self.ax1.set_xlabel("x [Å]")
+        self.ax1.set_ylabel("V [V]")
+        self.ax1.grid()
+        
+        # Tip Potential
+        self.ax2.clear()
+        self.ax2.imshow(data['Vtip'], extent=data['extent'], cmap='bwr', origin='lower', vmin=-params['VBias'], vmax=params['VBias'])
+        # Add circles for tip radius surface
+        zT = params['z_tip'] + params['Rtip']
+        circ1, _ = makeCircle(16, R=params['Rtip'], axs=(0,2,1), p0=(0.0,0.0,zT))
+        circ2, _ = makeCircle(16, R=params['Rtip'], axs=(0,2,1), p0=(0.0,0.0,2*params['zV0']-zT))
+        self.ax2.plot(circ1[:,0], circ1[:,2], ':k')
+        self.ax2.plot(circ2[:,0], circ2[:,2], ':k')
+        self.ax2.set_title("Tip Potential")
+        self.ax2.set_xlabel("x [Å]")
+        self.ax2.set_ylabel("z [Å]")
+        self.ax2.grid()
+        
+        # Site Potential
+        self.ax3.clear()
+        self.ax3.imshow(data['Esites'], extent=data['extent'], cmap='bwr', origin='lower', vmin=-params['VBias'], vmax=params['VBias'])
+        self.ax3.axhline(params['zV0'], ls='--', c='k', label='mirror surface')
+        self.ax3.axhline(params['zQd'], ls='--', c='g', label='Qdot height')
+        self.ax3.legend()
+        self.ax3.set_title("Site Potential")
+        self.ax3.set_xlabel("x [Å]")
+        self.ax3.set_ylabel("z [Å]")
+        self.ax3.grid()
+    
+    def plotQdotSystem(self):
+        """Plot X-Y projections using precomputed data"""
+        data = self.qdot_system_data
+        params = self.get_param_values()
+        
+        # Energies (using bwr colormap)
+        self.ax4.clear()
+        Es = compute_site_energies(data['ps_xy'], self.spos, VBias=params['Q_tip'], Rtip=1.0, zV0=params['z_tip'], E0s=self.Esite)
+        Es = Es.reshape(params['npix'], params['npix'], -1)
+        total_energy = np.sum(Es, axis=2)  # Sum over sites
+        im = self.ax4.imshow(total_energy, origin="lower", extent=data['extent'], cmap='bwr')
+        self.ax4.plot(self.spos[:,0], self.spos[:,1], '+g')
+        self.fig.colorbar(im, ax=self.ax4)
+        self.ax4.set_title("Energies")
+        self.ax4.set_xlabel("x [Å]")
+        self.ax4.set_ylabel("y [Å]")
+        
+        # Site Charge
+        self.ax5.clear()
+        im = self.ax5.imshow(data['total_charge'], origin="lower", extent=data['extent'])
+        self.ax5.plot(self.spos[:,0], self.spos[:,1], '+g')
+        self.fig.colorbar(im, ax=self.ax5)
+        self.ax5.set_title("Site Charge")
+        self.ax5.set_xlabel("x [Å]")
+        self.ax5.set_ylabel("y [Å]")
+        
+        # STM
+        self.ax6.clear()
+        im = self.ax6.imshow(data['STM'], origin="lower", extent=data['extent'])
+        self.ax6.plot(self.spos[:,0], self.spos[:,1], '+g')
+        self.fig.colorbar(im, ax=self.ax6)
+        self.ax6.set_title("STM")
+        self.ax6.set_xlabel("x [Å]")
+        self.ax6.set_ylabel("y [Å]")
+    
     def run_simulation(self):
         """Run simulation using Python backend"""
         self.init_simulation()
         params = self.get_param_values()
-        
-        # Setup scanning grid
-        npix = params['npix']
-        L = params['L']
         
         # Create 2x3 grid of plots
         self.fig.clear()
@@ -197,158 +308,18 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         self.ax5 = self.fig.add_subplot(gs[1, 1])  # STM
         self.ax6 = self.fig.add_subplot(gs[1, 2])  # dI/dQ
         
-        # Update all plots
-        self.update_plots(params)
-    
-    def update_plots(self, params):
-        """Update all 6 plots with current parameters"""
-        # Top row: X-Z projections
-        self.plot1DpotentialX(params)
-        self.plotTipPotentialXZ(params)
-        self.plotSitePotentialXZ(params)
+        # Perform calculations
+        self.calculateTipPotential(params)
+        self.calculateQdotSystem(params)
         
-        # Bottom row: X-Y projections
-        self.plotTotalCharge(params)
-        self.plotSTM(params)
-        self.plotDIdQ(params)
+        # Update all plots
+        self.plotTipPotential()
+        self.plotQdotSystem()
         
         # Adjust layout and draw
         self.fig.tight_layout()
         self.canvas.draw()
-    
-    def plot1DpotentialX(self, params):
-        """Plot 1D potential along x-axis"""
-        ps = np.zeros((params['npix'],3))
-        ps[:,0] = np.linspace(-params['L'], params['L'], params['npix'])
-        ps[:,2] = params['z_tip'] + params['Rtip']
-        
-        Vtip = tmul.compute_site_energies( ps, np.array([[0.0,0.0,params['zQd']]]),      params['VBias'], params['Rtip'], zV0=params['zV0']    )
-        
-        self.ax1.clear()
-        self.ax1.plot(ps[:,0], Vtip)
-        self.ax1.set_title("1D Potential")
-        self.ax1.set_xlabel("x [Å]")
-        self.ax1.set_ylabel("V [V]")
-        self.ax1.grid()
-    
-    def plotTipPotentialXZ(self, params):
-        """Plot 2D tip potential in X-Z plane"""
-        zT = params['z_tip'] + params['Rtip']
-        ps, Xs, Zs = tmul.makePosXY(n=params['npix'], L=params['L'], axs=(0,2,1))
-        
-        Vtip = tmul.compute_V_mirror( np.array([0.0,0.0,zT]), ps, VBias=params['VBias'], Rtip=params['Rtip'], zV0=params['zV0'] )
-        Vtip = Vtip.reshape(params['npix'], params['npix'])
-        
-        self.ax2.clear()
-        self.ax2.imshow(Vtip, extent=[-params['L'], params['L'], -params['L'], params['L']],
-                       cmap='bwr', origin='lower', vmin=-params['VBias'], vmax=params['VBias'])
-        self.ax2.set_title("Tip Potential")
-        self.ax2.set_xlabel("x [Å]")
-        self.ax2.set_ylabel("z [Å]")
-        self.ax2.grid()
-    
-    def plotSitePotentialXZ(self, params):
-        """Plot 2D site potential in X-Z plane"""
-        pSites = np.array([[0.0,0.0,params['zQd']]])
-        ps, Xs, Zs = tmul.makePosXY(n=params['npix'], L=params['L'], axs=(0,2,1))
-        
-        Esites = tmul.compute_site_energies( ps, pSites, params['VBias'], params['Rtip'], zV0=params['zV0'] )
-        Esites = Esites.reshape(params['npix'], params['npix'], len(pSites))
-        
-        self.ax3.clear()
-        self.ax3.imshow(Esites[:,:,0], extent=[-params['L'], params['L'], -params['L'], params['L']],
-                       cmap='bwr', origin='lower', vmin=-params['VBias'], vmax=params['VBias'])
-        self.ax3.axhline(params['zV0'], ls='--', c='k', label='mirror surface')
-        self.ax3.axhline(params['zQd'], ls='--', c='g', label='Qdot height')
-        self.ax3.legend()
-        self.ax3.set_title("Site Potential")
-        self.ax3.set_xlabel("x [Å]")
-        self.ax3.set_ylabel("z [Å]")
-        self.ax3.grid()
-    
-    def plotTotalCharge(self, params):
-        """Plot total charge distribution in X-Y plane"""
-        # Setup scanning grid
-        ps, Xs, Ys = tmul.makePosXY(n=params['npix'], L=params['L'], p0=(0,0,params['z_tip']))
-        
-        # Compute site energies and tunneling rates
-        Es = tmul.compute_site_energies( ps, self.spos, VBias=params['Q_tip'], Rtip=1.0, zV0=params['z_tip'], E0s=self.Esite )
-        
-        T = tmul.compute_site_tunelling( ps, self.spos, beta=params['decay'], Amp=1.0 )
-        T = T.reshape(params['npix'], params['npix'], -1)
-        
-        # Calculate charge distribution
-        Q = self.solve_occupancies(Es, T.reshape(-1, T.shape[-1]))
-        Q = Q.reshape(params['npix'], params['npix'], -1)
-        
-        # Plot total charge
-        total_charge = np.sum(Q, axis=2)
-        
-        self.ax4.clear()
-        im = self.ax4.imshow(total_charge, origin="lower", 
-                           extent=[-params['L'], params['L'], -params['L'], params['L']])
-        self.ax4.plot(self.spos[:,0], self.spos[:,1], '+g')
-        self.fig.colorbar(im, ax=self.ax4)
-        self.ax4.set_title("Total Charge")
-        self.ax4.set_xlabel("x [Å]")
-        self.ax4.set_ylabel("y [Å]")
-    
-    def plotSTM(self, params):
-        """Plot STM image in X-Y plane"""
-        # Setup scanning grid
-        ps, Xs, Ys = tmul.makePosXY(n=params['npix'], L=params['L'], p0=(0,0,params['z_tip']))
-        
-        # Compute site energies and tunneling rates
-        Es = tmul.compute_site_energies( ps, self.spos,  VBias=params['Q_tip'],  Rtip=1.0,  zV0=params['z_tip'], E0s=self.Esite  )
-        
-        T = tmul.compute_site_tunelling( ps, self.spos,  beta=params['decay'], Amp=1.0 )
-        T = T.reshape(params['npix'], params['npix'], -1)
-        
-        # Calculate charge distribution
-        Q = self.solve_occupancies(Es, T.reshape(-1, T.shape[-1]))
-        Q = Q.reshape(params['npix'], params['npix'], -1)
-        
-        # Compute STM image
-        I = self.getSTM_map(Q, T, params['dQ'])
-        
-        self.ax5.clear()
-        im = self.ax5.imshow(I, origin="lower", 
-                           extent=[-params['L'], params['L'], -params['L'], params['L']])
-        self.ax5.plot(self.spos[:,0], self.spos[:,1], '+g')
-        self.fig.colorbar(im, ax=self.ax5)
-        self.ax5.set_title("STM")
-        self.ax5.set_xlabel("x [Å]")
-        self.ax5.set_ylabel("y [Å]")
-    
-    def plotDIdQ(self, params):
-        """Plot dI/dQ in X-Y plane"""
-        # Setup scanning grid
-        ps, Xs, Ys = tmul.makePosXY(n=params['npix'], L=params['L'], p0=(0,0,params['z_tip']))
-        
-        # Compute site energies and tunneling rates
-        Es = tmul.compute_site_energies( ps, self.spos, VBias=params['Q_tip'],  Rtip=1.0,  zV0=params['z_tip'], E0s=self.Esite )
-        
-        T = tmul.compute_site_tunelling( ps, self.spos,  beta=params['decay'], Amp=1.0 )
-        T = T.reshape(params['npix'], params['npix'], -1)
-        
-        # Calculate charge distribution
-        Q = self.solve_occupancies(Es, T.reshape(-1, T.shape[-1]))
-        Q = Q.reshape(params['npix'], params['npix'], -1)
-        
-        # Compute STM image
-        I = self.getSTM_map(Q, T, params['dQ'])
-        
-        # Compute dI/dQ
-        dIdQ = (I - np.mean(I)) / params['dQ']
-        
-        self.ax6.clear()
-        im = self.ax6.imshow(dIdQ, origin="lower",   extent=[-params['L'], params['L'], -params['L'], params['L']])
-        self.ax6.plot(self.spos[:,0], self.spos[:,1], '+g')
-        self.fig.colorbar(im, ax=self.ax6)
-        self.ax6.set_title("dI/dQ")
-        self.ax6.set_xlabel("x [Å]")
-        self.ax6.set_ylabel("y [Å]")
-    
+
     def solve_occupancies(self, Es, T):
         """Calculate site occupancies using Fermi-Dirac statistics"""
         kT = 8.617e-5 * self.temperature  # eV/K
