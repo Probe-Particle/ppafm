c9439a528f710aa679c86abe360430d6a11d8c8b
NikoOinonen <42408893+NikoOinonen@users.noreply.github.com>
2025-02-05 16:53:01 +0200

    Update download links (#333)
    Change all Dropbox links to point to the new Zenodo archive.

--- examples/pyridineDensOverlap/run.sh
+++ examples/pyridineDensOverlap/run.sh
@@ -1,18 +1,24 @@
+#!/bin/bash
+
 echo "======= STEP 1 : Download the sample and tip files."
-wget https://zenodo.org/records/14222456/files/pyridine.zip?download=1 -O sample.zip
-wget https://zenodo.org/records/14222456/files/CO_tip.zip?download=1  -O tip.zip
+if [ ! -d "sample" ] ; then
+    wget "https://zenodo.org/records/14222456/files/pyridine.zip?download=1" -O sample.zip
+    unzip sample.zip -d sample
+    rm sample.zip
+fi
+if [ ! -d "tip" ] ; then
+    wget "https://zenodo.org/records/14222456/files/CO_tip.zip?download=1" -O tip.zip
+    unzip tip.zip -d tip
+    rm tip.zip
+fi
 
-echo "======= STEP 2 : Unzip the sample and tip files."
-unzip sample.zip
-unzip tip.zip -d tip
+echo "======= STEP 2 : Generate force field grid."
+ppafm-conv-rho       -s sample/CHGCAR.xsf -t tip/density_CO.xsf -B 1.0 -E
+ppafm-generate-elff  -i sample/LOCPOT.xsf --tip_dens tip/density_CO.xsf --Rcore 0.7 -E --doDensity
+ppafm-generate-dftd3 -i sample/LOCPOT.xsf --df_name PBE
 
-echo "======= STEP 3 : Generate force field grid."
-ppafm-conv-rho       -s CHGCAR.xsf -t tip/density_CO.xsf -B 1.0 -E
-ppafm-generate-elff  -i LOCPOT.xsf --tip_dens tip/density_CO.xsf --Rcore 0.7 -E --doDensity
-ppafm-generate-dftd3 -i LOCPOT.xsf --df_name PBE
-
-echo "======= STEP 4 : Relax Probe Particle using that force field grid."
+echo "======= STEP 3 : Relax Probe Particle using that force field grid."
 ppafm-relaxed-scan -k 0.25 -q 1.0 --noLJ --Apauli 18.0 --bDebugFFtot # Note the --noLJ for loading separate Pauli and vdW instead of LJ force field
 
-echo "======= STEP 5 : Plot the results."
+echo "======= STEP 4 : Plot the results."
 ppafm-plot-results -k 0.25 -q 1.0 -a 2.0 --df
