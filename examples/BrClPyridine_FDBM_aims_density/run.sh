c91441bfd220d0e23cb198a56aea87ef26c4cdc3
NikoOinonen <niko.second@pm.me>
2025-02-06 13:28:17 +0200

    Update link.

--- examples/BrClPyridine_FDBM_aims_density/run.sh
+++ examples/BrClPyridine_FDBM_aims_density/run.sh
@@ -9,22 +9,22 @@
 tip_dir="./tip"
 sample_dir="./sample"
 if [ ! -d "$tip_dir" ]; then
-    wget https://zenodo.org/records/10563098/files/CO_tip_densities.tar.gz
-    mkdir $tip_dir
-    tar -xvf CO_tip_densities.tar.gz --directory $tip_dir
+    wget "https://zenodo.org/records/14222456/files/CO_tip.zip?download=1" -O tip.zip
+    unzip tip.zip -d $tip_dir
+    rm tip.zip
 fi
 if [ ! -d "$sample_dir" ]; then
     wget "https://zenodo.org/records/14222456/files/pyridineBrCl.zip?download=1" -O hartree.zip
-    mkdir $sample_dir
     unzip hartree.zip -d $sample_dir
+    rm hartree.zip
 fi
 
 # Generate the force field
-# Notice here the `--density_cutoff` option. Try removing it and observe how artifacts appear in the image at the top-right
-# at the position of the bromine atom.
-ppafm-conv-rho       -s sample/density.xsf -t tip/density_CO.xsf -B 1.1 --density_cutoff 100
-ppafm-generate-elff  -i sample/hartree.xsf --tip dz2
-ppafm-generate-dftd3 -i sample/hartree.xsf --df_name PBE
+# Notice here the `--density_cutoff` option. Try removing it and observe how artifacts appear in
+# the image at the top-right at the position of the bromine atom.
+ppafm-conv-rho       -s $sample_dir/density.xsf -t $tip_dir/density_CO.xsf -B 1.1 #--density_cutoff 100
+ppafm-generate-elff  -i $sample_dir/hartree.xsf --tip dz2
+ppafm-generate-dftd3 -i $sample_dir/hartree.xsf --df_name PBE
 
 # Relax the probe particle in the force field
 ppafm-relaxed-scan -k 0.24 -q -0.1 --noLJ --Apauli 25.0
