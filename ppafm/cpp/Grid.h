38e7bf367e750f2a64db5a8c5f351eb5be2f0d2b
Martin Ondracek <ondracek@fzu.cz>
2026-02-10 15:05:52 +0100

    In interpolate3DWrap() and interpolate3DvecWrap(), replace a trick that used an arbitrary offset with an explicit special treatment of negative r.x, r.y, r.z Reformat the spacing in some functions from Grid.h

--- ppafm/cpp/Grid.h
+++ ppafm/cpp/Grid.h
@@ -13,9 +13,7 @@
 
 // ================= MACROS
 
-#define fast_floor_offset  1000
-#define fast_floor( x )    ( ( (int)( x + fast_floor_offset ) ) - fast_floor_offset )
-#define i3D( ix, iy, iz )  ( iz*nxy + iy*nx + ix  )
+#define i3D( ix, iy, iz )  ( (iz*ny + iy)*nx + ix  )
 
 // ================= CONSTANTS
 
@@ -68,76 +66,87 @@ class GridShape {
 
 // interpolation of vector force-field Vec3d[ix,iy,iz] in periodic boundary condition
 inline double interpolate3DWrap( double * grid, const Vec3i& n, const Vec3d& r ){
-    //#pragma omp simd
-    //{
-	int xoff = n.x<<3; int imx = r.x +xoff;	double tx = r.x - imx +xoff;	double mx = 1 - tx;		int itx = (imx+1)%n.x;  imx=imx%n.x;
-	int yoff = n.y<<3; int imy = r.y +yoff;	double ty = r.y - imy +yoff;	double my = 1 - ty;		int ity = (imy+1)%n.y;  imy=imy%n.y;
-	int zoff = n.z<<3; int imz = r.z +zoff;	double tz = r.z - imz +zoff;	double mz = 1 - tz;		int itz = (imz+1)%n.z;  imz=imz%n.z;
-	int nxy = n.x * n.y; int nx = n.x;
-	//double out = grid[ i3D( imx, imy, imz ) ];
+  //#pragma omp simd
+  //{
+  int nx = n.x; int ny = n.y; int nz = n.z;
+  int imx, imy, imz, itx, ity, itz;
+  double mx, my, mz, tx, ty, tz;
+  if(r.x >= 0) {imx = r.x; tx = r.x - imx; mx = 1 - tx; imx %= nx;}
+  else {itx = r.x; mx = itx - r.x; tx = 1 - mx; imx = itx % nx + nx - 1;}
+  itx = (imx + 1) % nx;
+  if(r.y >= 0) {imy = r.y; ty = r.y - imy; my = 1 - ty; imy %= ny;}
+  else {ity = r.y; my = ity - r.y; ty = 1 - my; imy = ity % ny + ny - 1;}
+  ity = (imy + 1) % ny;
+  if(r.z >= 0) {imz = r.z; tz = r.z - imz; mz = 1 - tz; imz %= nz;}
+  else {itz = r.z; mz = itz - r.z; tz = 1 - mz; imz = itz % nz + nz - 1;}
+  itz = (imz + 1) % nz;
 
-	//double out = mz * my * (  ( mx * grid[ i3D( imx, imy, imz ) ] ) +  ( tx * grid[ i3D( itx, imy, imz ) ] );
-	//ty * ( mx * grid[ i3D( imx, ity, imz ) ] ) +  ( tx * grid[ i3D( itx, ity, imz ) ] ) );
-
-	double out = mz * (
-	my * ( ( mx * grid[ i3D( imx, imy, imz ) ] ) +  ( tx * grid[ i3D( itx, imy, imz ) ] ) ) +
-	ty * ( ( mx * grid[ i3D( imx, ity, imz ) ] ) +  ( tx * grid[ i3D( itx, ity, imz ) ] ) ) )
-               + tz * (
-	my * ( ( mx * grid[ i3D( imx, imy, itz ) ] ) +  ( tx * grid[ i3D( itx, imy, itz ) ] ) ) +
-	ty * ( ( mx * grid[ i3D( imx, ity, itz ) ] ) +  ( tx * grid[ i3D( itx, ity, itz ) ] ) ) );
-    //}
-	return out;
+  double out =
+    mz * (
+	  my * ( ( mx * grid[ i3D( imx, imy, imz ) ] ) +  ( tx * grid[ i3D( itx, imy, imz ) ] ) ) +
+	  ty * ( ( mx * grid[ i3D( imx, ity, imz ) ] ) +  ( tx * grid[ i3D( itx, ity, imz ) ] ) ) ) +
+    tz * (
+	  my * ( ( mx * grid[ i3D( imx, imy, itz ) ] ) +  ( tx * grid[ i3D( itx, imy, itz ) ] ) ) +
+	  ty * ( ( mx * grid[ i3D( imx, ity, itz ) ] ) +  ( tx * grid[ i3D( itx, ity, itz ) ] ) ) );
+  //}
+  return out;
 }
 
 // interpolation of vector force-field Vec3d[ix,iy,iz] in periodic boundary condition
 inline Vec3d interpolate3DvecWrap( Vec3d * grid, const Vec3i& n, const Vec3d& r ){
-    //#pragma omp simd
-    //{
-	int xoff = n.x<<3; int imx = r.x +xoff;	double tx = r.x - imx +xoff;	double mx = 1 - tx;		int itx = (imx+1)%n.x;  imx=imx%n.x;
-	int yoff = n.y<<3; int imy = r.y +yoff;	double ty = r.y - imy +yoff;	double my = 1 - ty;		int ity = (imy+1)%n.y;  imy=imy%n.y;
-	int zoff = n.z<<3; int imz = r.z +zoff;	double tz = r.z - imz +zoff;	double mz = 1 - tz;		int itz = (imz+1)%n.z;  imz=imz%n.z;
-	int nxy = n.x * n.y; int nx = n.x;
-	double mymx = my*mx; double mytx = my*tx; double tymx = ty*mx; double tytx = ty*tx;
-	Vec3d out;
-	out.set_mul( grid[ i3D( imx, imy, imz ) ], mz*mymx );   out.add_mul( grid[ i3D( itx, imy, imz ) ], mz*mytx );
-	out.add_mul( grid[ i3D( imx, ity, imz ) ], mz*tymx );   out.add_mul( grid[ i3D( itx, ity, imz ) ], mz*tytx );
-	out.add_mul( grid[ i3D( imx, ity, itz ) ], tz*tymx );   out.add_mul( grid[ i3D( itx, ity, itz ) ], tz*tytx );
-	out.add_mul( grid[ i3D( imx, imy, itz ) ], tz*mymx );   out.add_mul( grid[ i3D( itx, imy, itz ) ], tz*mytx );
-	//printf( "DEBUG interpolate3DvecWrap gp(%g,%g,%g) igp(%i,%i,%i)/(%i,%i,%i) %i->%g out(%g,%g,%g) \n", r.x, r.y, r.z, imx, imy, imz, n.x,n.y,n.z, i3D( imx, imy, imz ), grid[ i3D( imx, imy, imz ) ], out.x,out.y,out.z );
-    //}
-	return out;
+  //#pragma omp simd
+  //{
+  int nx = n.x; int ny = n.y; int nz = n.z;
+  int imx, imy, imz, itx, ity, itz;
+  double mx, my, mz, tx, ty, tz;
+  if(r.x >= 0) {imx = r.x; tx = r.x - imx; mx = 1 - tx; imx %= nx;}
+  else {itx = r.x; mx = itx - r.x; tx = 1 - mx; imx = itx % nx + nx - 1;}
+  itx = (imx + 1) % nx;
+  if(r.y >= 0) {imy = r.y; ty = r.y - imy; my = 1 - ty; imy %= ny;}
+  else {ity = r.y; my = ity - r.y; ty = 1 - my; imy = ity % ny + ny - 1;}
+  ity = (imy + 1) % ny;
+  if(r.z >= 0) {imz = r.z; tz = r.z - imz; mz = 1 - tz; imz %= nz;}
+  else {itz = r.z; mz = itz - r.z; tz = 1 - mz; imz = itz % nz + nz - 1;}
+  itz = (imz + 1) % nz;
+
+  double mymx = my*mx; double mytx = my*tx; double tymx = ty*mx; double tytx = ty*tx;
+  Vec3d out;
+  out.set_mul( grid[ i3D( imx, imy, imz ) ], mz*mymx );   out.add_mul( grid[ i3D( itx, imy, imz ) ], mz*mytx );
+  out.add_mul( grid[ i3D( imx, ity, imz ) ], mz*tymx );   out.add_mul( grid[ i3D( itx, ity, imz ) ], mz*tytx );
+  out.add_mul( grid[ i3D( imx, ity, itz ) ], tz*tymx );   out.add_mul( grid[ i3D( itx, ity, itz ) ], tz*tytx );
+  out.add_mul( grid[ i3D( imx, imy, itz ) ], tz*mymx );   out.add_mul( grid[ i3D( itx, imy, itz ) ], tz*mytx );
+  //printf( "DEBUG interpolate3DvecWrap gp(%g,%g,%g) igp(%i,%i,%i)/(%i,%i,%i) %i->%g out(%g,%g,%g) \n", r.x, r.y, r.z, imx, imy, imz, n.x, n.y, n.z, i3D( imx, imy, imz ), grid[ i3D( imx, imy, imz ) ], out.x,out.y,out.z );
+  //}
+  return out;
 }
 
 // iterate over field
 template< void FUNC( int ibuff, const Vec3d& pos_, void * args ) >
 void iterateGrid3D( const Vec3d& pos0, const Vec3i& n, const Mat3d& dCell, void * args ){
-	int nx  = n.x; 	int ny  = n.y; 	int nz  = n.z;
-	//int nx  = n.z; 	int ny  = n.y; 	int nz  = n.x;
-	int nxy = ny * nx;
-	printf( "iterateGrid3D nx,y,z (%i,%i,%i) nxy %i\n", nx,ny,nz, nxy );
-	Vec3d pos;  pos.set( pos0 );
-	//printf(" iterateGrid3D : args %i \n", args );
-	for ( int ic=0; ic<nz; ic++ ){
-        std::cout << "ic " << ic;
-        std::cout.flush();
-        std::cout << '\r';
-		for ( int ib=0; ib<ny; ib++ ){
-	        for ( int ia=0; ia<nx; ia++ ){
-			    int ibuff = i3D( ia, ib, ic );
-                //FUNC( ibuff, {ia,ib,ic}, pos );
-                //pos = pos0 + dCell.c*ic + dCell.b*ib + dCell.a*ia;
-                FUNC( ibuff, pos, args );
-                //printf("(%i,%i,%i)(%3.3f,%3.3f,%3.3f)\n",ia,ib,ic,pos.x,pos.y,pos.z);
-				pos.add( dCell.a );
-			}
-			pos.add_mul( dCell.a, -nx );
-			pos.add( dCell.b );
-		}
-		//exit(0);
-		pos.add_mul( dCell.b, -ny );
-		pos.add( dCell.c );
-	}
-    printf ("\n");
+  int nx  = n.x; int ny  = n.y;	int nz  = n.z;
+  printf( "iterateGrid3D nx,y,z (%i,%i,%i)\n", nx, ny, nz);
+  Vec3d pos;  pos.set( pos0 );
+  //printf(" iterateGrid3D : args %i \n", args );
+  for ( int ic=0; ic<nz; ic++ ){
+    std::cout << "ic " << ic;
+    std::cout.flush();
+    std::cout << '\r';
+    for ( int ib=0; ib<ny; ib++ ){
+      for ( int ia=0; ia<nx; ia++ ){
+	int ibuff = i3D( ia, ib, ic );
+	//FUNC( ibuff, {ia,ib,ic}, pos );
+	//pos = pos0 + dCell.c*ic + dCell.b*ib + dCell.a*ia;
+	FUNC( ibuff, pos, args );
+	//printf("(%i,%i,%i)(%3.3f,%3.3f,%3.3f)\n",ia,ib,ic,pos.x,pos.y,pos.z);
+	pos.add( dCell.a );
+      }
+      pos.add_mul( dCell.a, -nx );
+      pos.add( dCell.b );
+    }
+    pos.add_mul( dCell.b, -ny );
+    pos.add( dCell.c );
+  }
+  printf ("\n");
 }
 
 template< void FUNC( int ibuff, const Vec3d& pos_, void * args ) >
